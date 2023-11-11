3632078798dafe02ad78a98463c8471d0f31c0b1
frkri <57220519+frkri@users.noreply.github.com>
2023-11-11 01:44:21 +0100

    Add browser check to previousConnections store

--- src/lib/client/stores/previousConnections.ts
+++ src/lib/client/stores/previousConnections.ts
@@ -1,8 +1,10 @@
+import { browser } from '$app/environment';
 import { writable } from 'svelte/store';
 
 export const previousConnections = writable<string[]>(
-	JSON.parse(localStorage.getItem('previousConnections') || '[]')
+	JSON.parse((browser && localStorage.getItem('previousConnections')) || '[]')
 );
-previousConnections.subscribe((value) => {
-	localStorage.setItem('previousConnections', JSON.stringify(value));
-});
+if (browser)
+	previousConnections.subscribe((value) => {
+		localStorage.setItem('previousConnections', JSON.stringify(value));
+	});
