679a769871c4a36a6ace1163eb389714fb396741
frkri <57220519+frkri@users.noreply.github.com>
2023-11-11 04:48:24 +0100

    Refactor server input component to use Connection class

--- src/lib/components/ServerInput.svelte
+++ src/lib/components/ServerInput.svelte
@@ -1,30 +1,30 @@
 <script lang="ts">
-	import { goto } from '$app/navigation';
-	import { page } from '$app/stores';
+	import { Connection } from '$lib/client/connection';
+	import { currentConnection } from '$lib/client/stores/currentConnection';
 	import { ArrowRight } from 'lucide-svelte';
-	import { fade } from 'svelte/transition';
 
-	export let url: string = '';
+	export let server: string = '';
 	let inputFailedValidation = false;
 
-	function navigate() {
-		if (url === '' || !url.includes(':')) return (inputFailedValidation = true);
-		let finalUrl = new URL($page.url + 'client');
-		finalUrl.searchParams.set('server', url);
-		goto(finalUrl);
+	function connect() {
+		if (server === '' || !server.includes(':')) return (inputFailedValidation = true);
+		let parts = server.split(':');
+		if (parts.length === 0) return (inputFailedValidation = true);
+
+		$currentConnection = new Connection({ ip: parts[0], port: Number.parseInt(parts[1]) });
 	}
 </script>
 
 <div id="server-input" class="flex gap-2">
 	<input
-		bind:value={url}
+		bind:value={server}
 		data-failed-validation={inputFailedValidation}
 		type="text"
 		class="flex-1 rounded border bg-smore-900 p-2 text-white data-[failed-validation=true]:border-red-500"
 		placeholder="localhost:443"
 	/>
 	<button
-		on:click={navigate}
+		on:click={connect}
 		class="flex w-14 items-center justify-center rounded border border-lapis-500 bg-lapis-500 outline-none focus-within:bg-transparent hover:bg-transparent"
 	>
 		<ArrowRight class="h-5 w-5 text-white" />
